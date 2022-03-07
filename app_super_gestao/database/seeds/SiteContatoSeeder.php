26cfbabdec54e8454afeab8dc2778f22a8802380
Hugo Barbosa Santos <hugocorone10@gmail.com>
2022-03-07 13:33:43 -0300

    Factories (semeando tabelas em massa com a dependência Faker)

--- app_super_gestao/database/seeds/SiteContatoSeeder.php
+++ app_super_gestao/database/seeds/SiteContatoSeeder.php
@@ -13,12 +13,14 @@ class SiteContatoSeeder extends Seeder
      */
     public function run()
     {
-        $contato = new SiteContato();
+       /* $contato = new SiteContato();
         $contato -> nome = str::random(10);
         $contato -> telefone = '('.rand(10, 99).') 9'.rand(10000000, 99999999);
         $contato -> email = strtolower($contato->nome).'@gmail.com';
         $contato -> motivo_contato = rand(0, 9);
         $contato -> mensagem = str::random(30);
-        $contato -> save();
+        $contato -> save();*/
+
+        factory(SiteContato::class, 100)->create();
     }
 }
