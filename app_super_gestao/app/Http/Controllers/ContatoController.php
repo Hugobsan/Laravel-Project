74f40783fae1d401db89e1bcd9f56d2bbeb3b118
Hugo Barbosa Santos <hugocorone10@gmail.com>
2022-03-16 07:38:48 -0300

    Customizando as mensagens de feedback de validação

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -14,14 +14,27 @@ class ContatoController extends Controller
         return view('site.contato', ['motivos_contato'=>$motivos_contato]);
     }
     public function salvar(Request $request){
-        //realizar a validação dos dados disponíveis no request
-        $request->validate([
+        $regras = [
             'nome' => 'min:2|max:40',
             'telefone' => 'required|unique:site_contatos',
             'email' => 'email|unique:site_contatos',
             'motivo_contatos_id' => 'required',
-            'mensagem' => 'required'
-        ]);
+            'mensagem' => 'required|max:2000'
+        ];
+
+        $feedback = [
+            'required'=>'O campo :attribute deve ser preenchido.',
+            'unique'=>'Este :attribute já está em uso.',
+            'nome.min'=>'O nome deve ter no mínimo 2 caracteres.',
+            'nome.max'=>'O nome deve ter no máximo 40 caracteres.',
+            'email'=>'O email informado não é válido.',
+            'mensagem'=>'A mensagem não pode ter mais que 2000 caracteres.',
+            'motivo_contatos_id.required' => 'É necessário escolher um motivo do contato.'   
+        ];
+
+        //realizar a validação dos dados disponíveis no request
+        $request->validate($regras,$feedback);
+        
         SiteContato::create($request->all());
         return redirect()->route('site.index');
     
