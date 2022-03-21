994658fd6e7482503acc5d33a6b25d3877eb508e
Hugo Barbosa Santos <hugocorone10@gmail.com>
2022-03-21 14:48:29 -0300

    142. Adicionando middlewares a um grupo de rotas

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', 'PrincipalController@principal')->name('site.index')->middleware('log.acesso');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
 
 Route::post('/', 'PrincipalController@principal')->name('site.index');
 
@@ -25,7 +25,7 @@ Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
 
 Route::get('/login', function(){ return 'Login'; })->name('site.login');
 
-Route::prefix('app')->group(function() {
+Route::prefix('app')->middleware('autenticacao')->group(function() {
     Route::get('/clientes', function(){ return 'Clientes'; })->name('app.clientes');
 
     Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
