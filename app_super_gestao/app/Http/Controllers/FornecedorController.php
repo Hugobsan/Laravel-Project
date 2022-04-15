32c47daab483325127ad2de6b0e8ccda5ee65671
Hugo Barbosa Santos <hugocorone10@gmail.com>
2022-04-15 10:18:05 -0300

    150. Implementando o menu de opções da área protegida da aplicação

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -2,50 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class FornecedorController extends Controller
 {
-    public function index(){
-        $fornecedores = [
-            0 => [
-                'nome' => 'Fornecedor 1',
-                'status' => 'N',
-                'cnpj' => '00.000.000/000-00',
-                'ddd' => '11', //São Paulo (SP)
-                'telefone' => '0000-0000'
-            ], 
-            1 => [
-                'nome' => 'Fornecedor 2',
-                'status' => 'S',
-                'cnpj' => null,
-                'ddd' => '85', //Fortaleza (CE)
-                'telefone' => '0000-0001'
-            ],  
-            2 => [
-                'nome' => 'Fornecedor 3',
-                'status' => 'S',
-                'cnpj' => null,
-                'ddd' => '32', //Juiz de Fora (MG)
-                'telefone' => '0000-0002'
-            ]
-        ];
-
-        $ddd = [
-            11 => [
-                'cidade' => 'São Paulo',
-                'estado' => 'SP'
-            ],
-            85 => [
-                'cidade' => 'Fortaleza',
-                'estado' => 'CE'
-            ],
-            32 => [
-                'cidade' => 'Juiz de Fora',
-                'estado' => 'MG'
-            ]
-        ];
-        
-        return view('app.fornecedor.index', compact('fornecedores', 'ddd'));
+    public function index() {
+        return view('app.fornecedor');
     }
 }
+
